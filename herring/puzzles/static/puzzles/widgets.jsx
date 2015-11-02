71566d5ea1f40a103bcbcdc65c251f3d6496f7a9
Karen Rustad Tölva <karen@aerofs.com>
2015-11-01 19:56:11 -0800

    Made widgets.jsx pass linting; fixed target generator for round IDs

--- herring/puzzles/static/puzzles/widgets.jsx
+++ herring/puzzles/static/puzzles/widgets.jsx
@@ -1,22 +1,33 @@
+'use strict';
+
 var interleave = function(element, array) {
-  retval = [];
-  for (var i = 0; i < array.length; i++) {
-    retval.push(array[i]);
-    if (i != array.length - 1) {
-      retval.push(element);
+    var retval = [];
+    for (var i = 0; i < array.length; i++) {
+        retval.push(array[i]);
+        if (i !== array.length - 1) {
+            retval.push(element);
+        }
     }
-  }
-  return retval;
+    return retval;
 };
+
+var targetifyRound = function(round) {
+    var s = '#R' + round.number + '-' + round.name;
+    return s.replace(/[ \%\$\@\!\(\)\\\/\[\]\^\&\*\?\.\,]+/gi, '-');
+};
+
 
 var NavHeaderComponent = React.createClass({
   render: function() {
-    var roundTags = this.props.rounds.map(function(round) {
-      var target = "#R" + round.number + "-" + round.name;
-      var key = 'shortcut-' + round.id.toString();
-      return <a key={key} href={target}>R{round.number}</a>
-    });
-    rs = interleave(" | ", roundTags);
+    var rs,
+        roundTags = this.props.rounds.map(function(round) {
+          var target = targetifyRound(round);
+          var key = 'shortcut-' + round.id.toString();
+          return (
+              <a key={key} href={target}>R{round.number}</a>
+          );
+        });
+    rs = interleave(' | ', roundTags);
     return (<div className="row">
           <div className="col-lg-12">
             <div className="shortcuts">
@@ -28,43 +39,6 @@ var NavHeaderComponent = React.createClass({
   }
 });
 
-var RoundsComponent = React.createClass({
-  render: function() {
-    var rs = this.props.rounds.map(function(round) {
-      return <RoundComponent round={round} />;
-    });
-    return <div>{rs.length > 0 ? {rs} : <p>No puzzles are available</p>}</div>
-  }
-});
-
-var RoundComponent = React.createClass({
-  render: function() {
-    var round = this.props.round;
-    var target = "R" + round.number + "-" + round.name;
-    var puzzles = round.puzzle_set.map(function(puzzle) {
-      return <PuzzleComponent puzzle={puzzle} />;
-    });
-    return (
-      <div key={round.id} className="row">
-        <div className="col-lg-12 round">
-          <h2 id={target}>R{round.number} {round.name}</h2>
-
-          <div className="col-lg-12">
-            <div className="row legend">
-              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4">
-                Name
-              </div>
-              <div className="col-xs-6 col-sm-3 col-md-3 col-lg-2">Answer</div>
-              <div className="visible-md visible-lg col-md-3 col-lg-4">Notes</div>
-              <div className="hidden-xs col-sm-3 col-md-2 col-lg-2">Tags</div>
-            </div>
-          </div>
-          {puzzles}
-        </div>
-      </div>
-      );
-  }
-});
 
 var PuzzleComponent = React.createClass({
   render: function() {
@@ -95,6 +69,47 @@ var PuzzleComponent = React.createClass({
   }
 });
 
+
+var RoundComponent = React.createClass({
+  render: function() {
+    var round = this.props.round;
+    var target = targetifyRound(round);
+    var puzzles = round.puzzle_set.map(function(puzzle) {
+      return <PuzzleComponent puzzle={puzzle} />;
+    });
+    return (
+      <div key={round.id} className="row">
+        <div className="col-lg-12 round">
+          <h2 id={target}>R{round.number} {round.name}</h2>
+
+          <div className="col-lg-12">
+            <div className="row legend">
+              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+                Name
+              </div>
+              <div className="col-xs-6 col-sm-3 col-md-3 col-lg-2">Answer</div>
+              <div className="visible-md visible-lg col-md-3 col-lg-4">Notes</div>
+              <div className="hidden-xs col-sm-3 col-md-2 col-lg-2">Tags</div>
+            </div>
+          </div>
+          {puzzles}
+        </div>
+      </div>
+      );
+  }
+});
+
+
+var RoundsComponent = React.createClass({
+  render: function() {
+    var rs = this.props.rounds.map(function(round) {
+      return <RoundComponent round={round} />;
+    });
+    return <div>{rs.length > 0 ? {rs} : <p>No puzzles are available</p>}</div>
+  }
+});
+
+
 var Page = React.createClass({
   getInitialState() {
     return {rounds: rounds};
@@ -109,4 +124,3 @@ var Page = React.createClass({
 
 var page = <Page />;
 var renderedPage = React.render(page, document.getElementById('react-root'));
-
