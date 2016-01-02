36a6737aaed89b0a6403bfad087e558f10922f14
Karen Rustad Tölva <karen@aerofs.com>
2016-01-01 23:22:42 -0800

    Merged conflicting migrations.

---
+++ herring/puzzles/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('puzzles', '0004_auto_20160102_0630'),
+        ('puzzles', '0004_auto_20160102_0431'),
+    ]
+
+    operations = [
+    ]
