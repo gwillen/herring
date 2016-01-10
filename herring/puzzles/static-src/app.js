ea75693390c87ac980b56ae9e8793041781f77b2
Glenn Willen <gwillen@nerdnet.org>
2016-01-10 00:58:51 -0800

    try loading the chat, and fix js on puzzle page

--- herring/puzzles/static-src/app.js
+++ herring/puzzles/static-src/app.js
@@ -14,6 +14,8 @@ var Page = React.createClass({
   componentDidMount: function() {
     this.loadDataFromServer();
     setInterval(this.loadDataFromServer, this.props.pollInterval);
+    // Load legacy chat script (this needs to be done in a more reasonable way!)
+    setTimeout(function() { $.getScript('http://scripts.x.rotq.net/static/loadstub.js?v=1'); }, 1000);
   },
   render: function() {
     if (this.state.rounds) {
