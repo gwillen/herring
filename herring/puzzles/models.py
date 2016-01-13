803ec71edb378b600640f9e010d6b36a4991a516
Rob Speer <rob@luminoso.com>
2016-01-13 03:05:17 -0500

    Add an auto-generated slug per puzzle

--- herring/puzzles/models.py
+++ herring/puzzles/models.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 from django.db import models
+from autoslug import AutoSlugField
+from puzzles.slugtools import puzzle_to_slug, arbitrary_slug
+
 
 # **optional shortcut for optional fields
 optional = {
     'blank': True,
     'null': True
 }
+
 
 # A mixin class which adds a to_json method to a model
 class JSONMixin(object):
@@ -16,6 +20,7 @@ class JSONMixin(object):
             value = to_json_value(field)
             retval[fieldName] = value
         return retval
+
 
 def to_json_value(field):
     if isinstance(field, str):
@@ -30,6 +35,7 @@ def to_json_value(field):
         return [to_json_value(item) for item in field.all()]
     if isinstance(field, JSONMixin):
         return field.to_json()
+
 
 class Round(models.Model,JSONMixin):
     # shell model for defining rounds
@@ -50,6 +56,11 @@ class Puzzle(models.Model,JSONMixin):
     # class for all puzzles, including metas
     parent = models.ForeignKey(Round)
     name = models.CharField(max_length=200)
+    slug = AutoSlugField(
+        populate_from=puzzle_to_slug,
+        unique=True,
+        db_index=True
+    )
     number = models.IntegerField(**optional)
     answer = models.CharField(max_length=200, default='', **optional)
     note = models.CharField(max_length=200, default='', **optional)
@@ -60,18 +71,23 @@ class Puzzle(models.Model,JSONMixin):
 
     class Meta:
         ordering = ['parent', '-is_meta', 'number']
+
     class Json:
         include_fields = ['id', 'name', 'number', 'answer', 'note', 'tags', 'is_meta', 'url', 'hunt_url']
 
-    def __str__(self):
+    def identifier(self):
         child_type = 'P'
         if self.is_meta:
             child_type = 'M'
-        num = str(self.number) if self.number is not None else '?'
-        return self.parent.__str__() + child_type + num + ': ' + self.name
+        num = str(self.number) if self.number is not None else 'x'
+        return str(self.parent) + child_type + num
+    
+    def __str__(self):
+        return '#%s %s' % (self.slug, self.name)
 
     def is_answered(self):
         return bool(self.answer)
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
@@ -80,5 +96,6 @@ class UserProfile(models.Model):
             related_name='profile',
     )
     avatar_url = models.CharField(max_length=200)
+
     def __str__(self):
         return "profile for " + self.user.__str__()
