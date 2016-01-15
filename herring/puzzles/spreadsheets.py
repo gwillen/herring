b877a1a125c68d049a7b0d4b3223154276303d4c
Rob Speer <rob@luminoso.com>
2016-01-15 02:21:23 -0500

    shorten spreadsheet URLs

--- herring/puzzles/spreadsheets.py
+++ herring/puzzles/spreadsheets.py
@@ -1,6 +1,7 @@
 from googleapiclient.discovery import build
 from oauth2client.client import SignedJwtAssertionCredentials
 import httplib2
+import requests
 
 service = build('drive', 'v2')
 
@@ -13,8 +14,16 @@ try:
         'https://www.googleapis.com/auth/drive'
     )
     http = credentials.authorize(httplib2.Http())
+    shortener_url = 'https://www.googleapis.com/urlshortener/v1/url?key={}'.format(SECRETS['google-api-key'])
 except KeyError:
     http = None
+    shortener_url = 'https://www.googleapis.com/urlshortener/v1/url'
+
+
+def shorten_url(url):
+    response = requests.post(shortener_url, json={'longUrl': url})
+    short_url = response.json()['id']
+    return short_url
 
 
 def make_sheet(title):
@@ -27,4 +36,5 @@ def make_sheet(title):
 
     # I don't know why it's called "alternateLink", but it's the link that
     # works.
-    return got['alternateLink']
+    long_url = got['alternateLink']
+    return shorten_url(long_url)
